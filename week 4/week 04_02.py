05794f7563cf6f0cc5336b5ef7c4638d31a937e1
Leto Shelby <57267012+letoshelby@users.noreply.github.com>
2019-11-14 20:22:43 +0700

    Task: Дескриптор с комиссией

---
+++ week 4/week 04_02.py
@@ -0,0 +1,18 @@
+class Value:
+
+	def __get__(self, instance, owner):
+		return self.value
+
+	def __set__(self, object, value):
+		self.value = value - value * object.commission
+
+
+class Account:
+	amount = Value()
+
+	def __init__(self, commission):
+		self.commission = commission
+
+
+
+
